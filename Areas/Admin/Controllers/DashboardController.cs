4965e6fc3185f25c831e307391f1137cf5c20787
JulianYe <9ulianye@gmail.com>
2026-03-22 20:57:34 +0800

    feat(dashboard): 新增後台 Dashboard Controller 與 Index View

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TayanaYachtMVC.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
