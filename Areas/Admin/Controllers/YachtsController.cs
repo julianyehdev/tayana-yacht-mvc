d908ba3cc1c484d67a511218ba7b3cc1e3aeef09
JulianYe <9ulianye@gmail.com>
2026-03-25 23:27:15 +0800

    feat(admin-yacht): 新增後台遊艇 CRUD（Controller + Views）

---
+++ Areas/Admin/Controllers/YachtsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TayanaYachtMVC.Data;
+using TayanaYachtMVC.Models.Domain;
+
+namespace TayanaYachtMVC.Areas.Admin.Controllers
+{
+    public class YachtsController : Controller
+    {
+        private TayanaYachtDBContext db = new TayanaYachtDBContext();
+
+        // GET: Admin/Yachts
+        public ActionResult Index()
+        {
+            return View(db.Yachts.ToList());
+        }
+
+        // GET: Admin/Yachts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yacht yacht = db.Yachts.Find(id);
+            if (yacht == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yacht);
+        }
+
+        // GET: Admin/Yachts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Yachts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "YachtID,YachtName,IsLatest,CreatedAt,UpdatedAt")] Yacht yacht)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Yachts.Add(yacht);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(yacht);
+        }
+
+        // GET: Admin/Yachts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yacht yacht = db.Yachts.Find(id);
+            if (yacht == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yacht);
+        }
+
+        // POST: Admin/Yachts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "YachtID,YachtName,IsLatest,CreatedAt,UpdatedAt")] Yacht yacht)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(yacht).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(yacht);
+        }
+
+        // GET: Admin/Yachts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yacht yacht = db.Yachts.Find(id);
+            if (yacht == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yacht);
+        }
+
+        // POST: Admin/Yachts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Yacht yacht = db.Yachts.Find(id);
+            db.Yachts.Remove(yacht);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
