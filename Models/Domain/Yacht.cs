d63299adf360f2d005ea9dc0183563cca7cbda6a
JulianYe <9ulianye@gmail.com>
2026-03-26 00:08:46 +0800

    feat(admin-yacht): 中文化遊艇 Model Display 名稱及 Index View 文字

--- Models/Domain/Yacht.cs
+++ Models/Domain/Yacht.cs
@@ -18,12 +18,16 @@ namespace TayanaYachtMVC.Models.Domain
 
         [Required]
         [StringLength(100)]
+        [Display(Name = "遊艇名稱")]
         public string YachtName { get; set; }
 
+        [Display(Name = "最新款")]
         public bool IsLatest { get; set; }
 
+        [Display(Name = "建立時間")]
         public DateTime CreatedAt { get; set; }
 
+        [Display(Name = "更新時間")]
         public DateTime UpdatedAt { get; set; }
     }
 }
