e29fc1eacf1b8c483e0918659910a7fe88ab5782
JulianYe <9ulianye@gmail.com>
2026-03-26 16:25:09 +0800

    feat(yachts): 在新增頁加入規格示意圖上傳欄位

--- Migrations/202603260750452_AddYachtDimensionsImgUrl.cs
+++ Migrations/202603260750452_AddYachtDimensionsImgUrl.cs
@@ -1,15 +1,14 @@
 ﻿namespace TayanaYachtMVC.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddYachtDimensionsImgUrl : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Yachts", "DimensionsImgUrl", c => c.String());
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.Yachts", "DimensionsImgUrl");
