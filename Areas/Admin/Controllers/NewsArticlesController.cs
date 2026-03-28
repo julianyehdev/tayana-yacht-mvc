24a1509d115b70aec713e2d4d9cfe573f5c08f61
JulianYe <9ulianye@gmail.com>
2026-03-29 02:08:51 +0800

    feat(news): 新增後台新聞 CRUD（Controller + Views）

---
+++ Areas/Admin/Controllers/NewsArticlesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TayanaYachtMVC.Data;
+using TayanaYachtMVC.Models.Domain;
+
+namespace TayanaYachtMVC.Areas.Admin.Controllers
+{
+    public class NewsArticlesController : Controller
+    {
+        private TayanaYachtDBContext db = new TayanaYachtDBContext();
+
+        // GET: Admin/NewsArticles
+        public ActionResult Index()
+        {
+            var newsArticles = db.NewsArticles.Include(n => n.Category);
+            return View(newsArticles.ToList());
+        }
+
+        // GET: Admin/NewsArticles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewsArticle newsArticle = db.NewsArticles.Find(id);
+            if (newsArticle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newsArticle);
+        }
+
+        // GET: Admin/NewsArticles/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.NewsCategories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/NewsArticles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,CoverImageUrl,Content,PublishDate,IsPublished,IsPinned,CategoryId")] NewsArticle newsArticle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NewsArticles.Add(newsArticle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.NewsCategories, "Id", "Name", newsArticle.CategoryId);
+            return View(newsArticle);
+        }
+
+        // GET: Admin/NewsArticles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewsArticle newsArticle = db.NewsArticles.Find(id);
+            if (newsArticle == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.NewsCategories, "Id", "Name", newsArticle.CategoryId);
+            return View(newsArticle);
+        }
+
+        // POST: Admin/NewsArticles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,CoverImageUrl,Content,PublishDate,IsPublished,IsPinned,CategoryId")] NewsArticle newsArticle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(newsArticle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.NewsCategories, "Id", "Name", newsArticle.CategoryId);
+            return View(newsArticle);
+        }
+
+        // GET: Admin/NewsArticles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewsArticle newsArticle = db.NewsArticles.Find(id);
+            if (newsArticle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newsArticle);
+        }
+
+        // POST: Admin/NewsArticles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            NewsArticle newsArticle = db.NewsArticles.Find(id);
+            db.NewsArticles.Remove(newsArticle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
