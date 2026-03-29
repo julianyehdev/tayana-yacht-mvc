b6eb1513b74ef7d5f5254857618c7cd2ef5c8d18
JulianYe <9ulianye@gmail.com>
2026-03-30 02:38:23 +0800

    feat(yacht): 串接 Overview 頁面輪播圖至資料庫相簿，動態讀取左欄 Yachts 選單

--- Controllers/YachtController.cs
+++ Controllers/YachtController.cs
@@ -1,25 +1,43 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using TayanaYachtMVC.Data;
 
 namespace TayanaYachtMVC.Controllers
 {
     public class YachtController : Controller
     {
+        private readonly TayanaYachtDBContext _context;
+
+        public YachtController()
+        {
+            _context = new TayanaYachtDBContext();
+        }
+
         // GET: Yacht
         public ActionResult Overview(string id)
         {
             // id = "Tayana 58" or "Tayana 78" or "Tayana 84"
+            var yacht = string.IsNullOrEmpty(id)
+                ? _context.Yachts.Include(y => y.YachtPhotos).FirstOrDefault()
+                : _context.Yachts.Include(y => y.YachtPhotos).FirstOrDefault(y => y.YachtName == id);
+
+            if (yacht == null)
+                return HttpNotFound();
+
+            ViewBag.AllYachts = _context.Yachts.OrderBy(y => y.YachtID).ToList();
+            return View(yacht);
+        }
+
+        public ActionResult Layout(string _id)
+        {
+            // _id = "Tayana 58" or "Tayana 78" or "Tayana 84"
             return View();
         }
 
-        public ActionResult Layout(string id)
+        public ActionResult Specification(string _id)
         {
-            // id = "Tayana 58" or "Tayana 78" or "Tayana 84"
-            return View();
-        }
-
-        public ActionResult Specification(string id)
-        {
-            // id = "Tayana 58" or "Tayana 78" or "Tayana 84"
+            // _id = "Tayana 58" or "Tayana 78" or "Tayana 84"
             return View();
         }
 
