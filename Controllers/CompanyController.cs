328929a7d1db229719b528de399473e503a8f5ff
JulianYe <9ulianye@gmail.com>
2026-03-30 00:33:06 +0800

    feat(company): 新增 Company 頁面基礎結構（Controller + View）

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace TayanaYachtMVC.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
