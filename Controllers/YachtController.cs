73350e7e433f57b579a89054735da187ffd70d46
JulianYe <9ulianye@gmail.com>
2026-03-30 02:02:33 +0800

    feat(yacht): 新增 Yacht 頁面基礎結構（Controller + Views）

---
+++ Controllers/YachtController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace TayanaYachtMVC.Controllers
+{
+    public class YachtController : Controller
+    {
+        // GET: Yacht
+        public ActionResult Overview(string id)
+        {
+            // id = "Tayana 58" or "Tayana 78" or "Tayana 84"
+            return View();
+        }
+
+        public ActionResult Layout(string id)
+        {
+            // id = "Tayana 58" or "Tayana 78" or "Tayana 84"
+            return View();
+        }
+
+        public ActionResult Specification(string id)
+        {
+            // id = "Tayana 58" or "Tayana 78" or "Tayana 84"
+            return View();
+        }
+
+    }
+}
