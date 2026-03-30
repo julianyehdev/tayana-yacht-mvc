fb5919c376e7d22a575d3d9ebf38b9b16b3a6bb6
JulianYe <9ulianye@gmail.com>
2026-03-30 15:35:24 +0800

    feat(news): News Index 整合資料庫查詢與分頁

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -1,13 +1,47 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using TayanaYachtMVC.Data;
+using TayanaYachtMVC.Models.Domain;
 
 namespace TayanaYachtMVC.Controllers
 {
     public class NewsController : Controller
     {
-        // GET: News
-        public ActionResult Index()
+        private readonly TayanaYachtDBContext _context;
+
+        public NewsController()
         {
-            return View();
+            _context = new TayanaYachtDBContext();
+        }
+
+        // GET: News
+        public ActionResult Index(int page = 1)
+        {
+            const int pageSize = 5;
+
+            var query = _context.NewsArticles
+                .Where(x => x.IsPublished)
+                .OrderByDescending(x => x.IsPinned)
+                .ThenByDescending(x => x.PublishDate);
+
+            var totalCount = query.Count();
+            var totalPages = (totalCount + pageSize - 1) / pageSize;
+
+            // 驗證頁碼有效性
+            if (page < 1) page = 1;
+            if (page > totalPages && totalPages > 0) page = totalPages;
+
+            var articles = query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.TotalCount = totalCount;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageSize = pageSize;
+
+            return View(articles);
         }
 
         public ActionResult Article(string id)
