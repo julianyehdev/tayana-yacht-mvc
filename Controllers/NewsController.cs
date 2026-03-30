1a8ad2c315475a6dbaba064b35081885807d6274
JulianYe <9ulianye@gmail.com>
2026-03-30 14:54:23 +0800

    feat(news): 新增 News Controller 和檢視資料夾

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace TayanaYachtMVC.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Article(string id)
+        {
+            return View();
+        }
+
+
+    }
+}
