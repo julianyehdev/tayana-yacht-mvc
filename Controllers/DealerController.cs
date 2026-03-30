837538744d48e89d6fdddfca02510a149787649d
JulianYe <9ulianye@gmail.com>
2026-03-30 12:39:58 +0800

    feat(dealer): 動態化 Dealer Index 頁面，支持按國家篩選

--- Controllers/DealerController.cs
+++ Controllers/DealerController.cs
@@ -1,17 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using TayanaYachtMVC.Data;
 
 namespace TayanaYachtMVC.Controllers
 {
     public class DealerController : Controller
     {
-        // GET: Dealer
-        public ActionResult Index()
+        private readonly TayanaYachtDBContext _context;
+
+        public DealerController()
         {
-            return View();
+            _context = new TayanaYachtDBContext();
+        }
+
+        // GET: Dealer
+        public ActionResult Index(int? countryId = null)
+        {
+            IQueryable<Dealer> query = _context.Dealers
+                .Include(d => d.Region)
+                .Include(d => d.Region.Country)
+                .Where(d => d.IsPublished);
+
+            if (countryId.HasValue)
+            {
+                query = query.Where(d => d.Region.CountryId == countryId);
+            }
+
+            var dealers = query.OrderBy(d => d.SortOrder).ToList();
+
+            ViewBag.AllCountries = _context.Countries.OrderBy(c => c.SortOrder).ToList();
+
+            // 動態設定麵包屑顯示的國家名稱
+            if (countryId.HasValue)
+            {
+                var country = _context.Countries.FirstOrDefault(c => c.Id == countryId);
+                ViewBag.CurrentCountryName = country?.CountryName ?? "Dealers";
+            }
+            else
+            {
+                ViewBag.CurrentCountryName = "Dealers";
+            }
+
+            return View(dealers);
         }
     }
 }
