7d994b132030be8afacf974284a1246a97430ca4
JulianYe <9ulianye@gmail.com>
2026-04-03 19:25:34 +0800

    feat(dealer): 實作經銷商列表分頁功能

--- Controllers/DealerController.cs
+++ Controllers/DealerController.cs
@@ -16,8 +16,10 @@ namespace TayanaYachtMVC.Controllers
         }
 
         // GET: Dealer
-        public ActionResult Index(int? countryId = null)
+        public ActionResult Index(int? countryId = null, int page = 1)
         {
+            const int pageSize = 5;
+
             IQueryable<Dealer> query = _context.Dealers
                 .Include(d => d.Region)
                 .Include(d => d.Region.Country)
@@ -28,9 +30,26 @@ namespace TayanaYachtMVC.Controllers
                 query = query.Where(d => d.Region.CountryId == countryId);
             }
 
-            var dealers = query.OrderBy(d => d.SortOrder).ToList();
+            var orderedQuery = query.OrderBy(d => d.SortOrder);
+
+            var totalCount = orderedQuery.Count();
+            var totalPages = (totalCount + pageSize - 1) / pageSize;
+
+            // 驗證頁碼有效性
+            if (page < 1) page = 1;
+            if (page > totalPages && totalPages > 0) page = totalPages;
+
+            var dealers = orderedQuery
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
 
             ViewBag.AllCountries = _context.Countries.OrderBy(c => c.SortOrder).ToList();
+            ViewBag.CountryId = countryId;
+            ViewBag.TotalCount = totalCount;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageSize = pageSize;
 
             // 動態設定麵包屑顯示的國家名稱
             if (countryId.HasValue)
