a296273a4f9a69d8b19ed880129063c5a42bbe9c
JulianYe <9ulianye@gmail.com>
2026-04-03 19:36:09 +0800

    feat(contact): 將聯繫表單下拉選單改為動態資料

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TayanaYachtMVC.Data;
+using TayanaYachtMVC.Models.Domain;
 
 namespace TayanaYachtMVC.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly TayanaYachtDBContext _context;
+
+        public ContactController()
+        {
+            _context = new TayanaYachtDBContext();
+        }
+
         // GET: Contact
         public ActionResult Index()
         {
+            var countries = _context.Countries.OrderBy(c => c.SortOrder).ToList();
+            var yachts = _context.Yachts.OrderBy(y => y.YachtName).ToList();
+
+            ViewBag.Countries = countries;
+            ViewBag.Yachts = yachts;
+
             return View();
         }
     }
